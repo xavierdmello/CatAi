2abc8a71b1a40f56a9217669fc2970d1f3ff58b2
Xavier D'Mello <xaviermdmello@gmail.com>
2023-12-26 16:19:07 -0500

    add example images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastai.vision.all import *
 import gradio as gr
+import os
 
 # Load your model
 learn = load_learner('model.pkl')
@@ -11,15 +12,18 @@ def predict(img):
     pred, pred_idx, probs = learn.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
-# Example images (replace with your actual file paths or URLs)
-example_images = ["./example_images"]
+# Directory containing example images
+example_images_dir = "./example_images/"
+
+# Generate list of file paths for example images
+example_images = [os.path.join(example_images_dir, file) for file in os.listdir(example_images_dir) if file.endswith(('jpg', 'jpeg', 'png', 'JPG'))]
 
 # Create Gradio interface using components
 interface = gr.Interface(
     fn=predict, 
     inputs=gr.components.Image(), 
-    outputs=gr.components.Label(num_top_classes=3),
-    examples=example_images  # Added example images
+    outputs=gr.components.Label(),
+    examples=example_images 
 )
 
 # Launch the interface
