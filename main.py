3fe375baef59f26de2988bb08e6511e9d27a02c4
Xavier D'Mello <xaviermdmello@gmail.com>
2023-12-26 16:16:03 -0500

    add example images

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from fastai.vision.all import *
 import gradio as gr
 
-# Load  model
+# Load your model
 learn = load_learner('model.pkl')
 labels = learn.dls.vocab
 
-# Define prediction function
+# Define your prediction function
 def predict(img):
     img = PILImage.create(img)
     pred, pred_idx, probs = learn.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
+# Example images (replace with your actual file paths or URLs)
+example_images = ["./example_images"]
+
 # Create Gradio interface using components
 interface = gr.Interface(
     fn=predict, 
-    inputs=gr.components.Image(),  # Updated this line
-    outputs=gr.components.Label()
+    inputs=gr.components.Image(), 
+    outputs=gr.components.Label(num_top_classes=3),
+    examples=example_images  # Added example images
 )
 
-# Launch interface
+# Launch the interface
 interface.launch(share=True)
